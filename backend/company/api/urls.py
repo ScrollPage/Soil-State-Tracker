f9a7a2a8f253bd3eca102debcc4b16dfa0c68bb7
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-11-03 16:32:01 +0300

    Urls fix

--- backend/company/api/urls.py
+++ backend/company/api/urls.py
@@ -28,14 +28,24 @@ remove_detectors = views.CompanyViewSet.as_view({
     'post': 'remove_detectors'
 })
 
+company = views.CompanyViewSet.as_view({
+    'post': 'create',
+    'get': 'list'
+})
+
+company_detail = views.CompanyViewSet.as_view({
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+    'get': 'retrieve'
+})
+
 urlpatterns += format_suffix_patterns([
     path('company/<int:pk>/workers/', workers_list, name='workers-list'),
     path('company/<int:pk>/detectors/', detectors_list, name='detectors-list'),
     path('company/<int:pk>/transfer_detectors/', detectors_transfer_list, name='detectors-transfer-list'),
     path('company/<int:pk>/detectors/add/', add_detectors, name='add-detectors'),
     path('company/<int:pk>/detectors/remove/', remove_detectors, name='remove-detectors'),
+    path('company/', company, name='company-list'),
+    path('company/<int:pk>/', company_detail, name='company-detail'),
 ])
-
-r = DefaultRouter()
-r.register('company', views.CompanyViewSet, basename='company')
-urlpatterns += r.urls
