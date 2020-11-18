fae81e1eb736260c7131e9838cb033ab2acfc613
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-11-18 15:32:33 +0300

    Fix

--- backend/chat/service.py
+++ backend/chat/service.py
@@ -7,7 +7,8 @@ def send_mass_notifications(instance, channel_name, event, exclude_manager=False
 		lambda manager: settings.pusher_client.trigger(
 			f'{channel_name}{manager.id}', 
 			event,
-			{'chat': instance.id, 'user_name': instance.user_name}
+			{'chat': instance.id, 'user_name': instance.user_name} if event == 'new_chat' \
+				else {'chat': instance.id}
 		),
 		Client.objects.filter(is_staff=True) if not exclude_manager else \
 			Client.objects.filter(is_staff=True).exclude(id=instance.manager.id)
