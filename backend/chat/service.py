5bff67ba0be45233e99ef8eacb0c6f53029a4876
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-11-21 21:19:15 +0300

    Chat notifications fix

--- backend/chat/service.py
+++ backend/chat/service.py
@@ -2,24 +2,17 @@ from backend import settings
 
 from client.models import Client
 
+def send_notification(manager, instance, channel_name, event):
+	settings.pusher_client.trigger(
+		f'{channel_name}{manager.id}', 
+		event,
+		{'chat': instance.id, 'user_name': instance.user_name} if event == 'new_chat' \
+			else {'chat': instance.id}
+	)
+
 def send_mass_notifications(instance, channel_name, event, exclude_manager=False):
-	map(
-		lambda manager: settings.pusher_client.trigger(
-			f'{channel_name}{manager.id}', 
-			event,
-			{'chat': instance.id, 'user_name': instance.user_name} if event == 'new_chat' \
-				else {'chat': instance.id}
-		),
+	list(map(
+		lambda manager: send_notification(manager, instance,  channel_name, event),
 		Client.objects.filter(is_staff=True) if not exclude_manager else \
 			Client.objects.filter(is_staff=True).exclude(id=instance.manager.id)
-	)
-	# print(channel_name, event, exclude_manager)
-	# for manager in Client.objects.filter(is_staff=True) if not exclude_manager else \
-	# 	Client.objects.filter(is_staff=True).exclude(id=instance.manager.id):
-
-	# 	settings.pusher_client.trigger(
-	# 		f'{channel_name}{manager.id}', 
-	# 		event,
-	# 		{'chat': instance.id, 'user_name': instance.user_name} if event == 'new_chat' \
-	# 			else {'chat': instance.id}
-	# 	)
+	))
