67225a12426523e3f6c81534bb3bb57f765c8ac8
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-11-25 23:26:27 +0300

    Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,23 +94,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Soil-State-Tracker',
-#         'USER': 'postgres',
-#         'PASSWORD': 'pass',
-#         'HOST': '127.0.0.1',
-#         'PORT': 5432
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Soil-State-Tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
+    }
+}
 
 
 # Password validation
--- backend/company/api/service.py
+++ backend/company/api/service.py
@@ -16,6 +16,9 @@ def join_qs(queryset, objects):
     return queryset.join(objects)
 
 def get_detectors_out_of_company_qs(queryset):
-    detectors_queryset = queryset.first().detectors.all()
+    try:
+        detectors_queryset = queryset.first().detectors.all()
+    except AttributeError:
+        return Detector.objects.none()
     list(map(lambda company: join_qs(detectors_queryset, company.detectors.all()), queryset[1:]))
     return detectors_queryset
